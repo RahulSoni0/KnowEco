7e4501e60d31e9b15f69ddc85cf92e3e2978fc5c
Subhadiptech <2020ugee055@nitjsr.ac.in>
2021-10-24 01:19:27 +0530

    added TextWatchers  in CreatePost fragment

--- app/src/main/java/com/example/toastapp/Fragments/CreatePost.java
+++ app/src/main/java/com/example/toastapp/Fragments/CreatePost.java
@@ -8,6 +8,8 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,9 @@ import android.widget.EditText;
 
 import com.example.toastapp.R;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreatePost extends Fragment {
 
@@ -59,6 +64,117 @@ public class CreatePost extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Map<String,Object> newPost=new HashMap<>();
+
+
+        //Implementing Text Watchers
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        title.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                checkInputs();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        //end
+
+
+
+
+
+
+
+
+        //postButton
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo:send object to db
+                //intent to thanks fragment
+            }
+        });
+        //end
+    }
+
+    private void checkInputs(){
+
+
+
+        if(!name.getText().toString().trim().equals("")){
+
+            if(!email.getText().toString().trim().equals("")){
+
+                if (!title.getText().toString().trim().equals("")){
+
+
+                    post.setEnabled(true);
+
+
+                }else{
+
+                    title.setError("Please give some title!");
+                    post.setEnabled(false);
+
+                }
+
+            }else{
+                email.setError("Please enter your email!");
+                post.setEnabled(false);
+
+            }
+
+
+        }else{
+
+            name.setError("Please enter your name!");
+            post.setEnabled(false);
+
+        }
 
     }
 }
