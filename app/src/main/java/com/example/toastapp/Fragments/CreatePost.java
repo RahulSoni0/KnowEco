1aac235e134e9cd717a0b59cd7c8309b9d4b56be
Subhadiptech <2020ugee055@nitjsr.ac.in>
2021-10-24 01:07:44 +0530

    initialized view in CreatePost fragment

--- app/src/main/java/com/example/toastapp/Fragments/CreatePost.java
+++ app/src/main/java/com/example/toastapp/Fragments/CreatePost.java
@@ -2,15 +2,25 @@ package com.example.toastapp.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.toastapp.R;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CreatePost extends Fragment {
+
+    private EditText name,email,title,description;
+    private AppCompatButton post;
+    FirebaseFirestore store;
 
 
 
@@ -34,8 +44,21 @@ public class CreatePost extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_create_post, container, false);
-
-
+        //initialisation
+        post=view.findViewById(R.id.add_post);
+        name=view.findViewById(R.id.nameEt);
+        email=view.findViewById(R.id.emailEt);
+        title=view.findViewById(R.id.titleEt);
+        description=view.findViewById(R.id.descriptionEt);
+        store=FirebaseFirestore.getInstance();
+        //end
         return  view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
